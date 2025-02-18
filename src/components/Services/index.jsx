f6ffd1a1b2a254dae9b2067ae6c46a1ce482caa2
unknown <marcio.b.a.dias@gmail.com>
2025-02-18 15:25:10 -0300

    ✔ Ajustando useEffect para rendereizar infos da sessão Services

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -16,19 +16,29 @@ const initialState = {
 const Services = () => {
   const [status, dispatch] = useReducer( reduce, initialState)
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/MarcioBADias/data-fake/refs/heads/main/infos.json')
-    .then(r => r.json())
-    .then((data) => dispatch({ type: 'set_infos' }, data))
-    .catch(err => console.log(err))
-    console.log(status.infos)
+    const fetchData = async () => {
+      try{
+        const response = await fetch('https://raw.githubusercontent.com/MarcioBADias/data-fake/refs/heads/main/infos.json')
+        const data = await response.json()
+        dispatch({type: 'set_infos', infos: data})
+      } catch (err) {
+        console.error('Erro ao carregar a API: ', err)
+      }
+    }
+    fetchData()
   }, [])
 
   return (
     <Container name="services" id="services">
       <div>
-            <h1 style={{ marginBottom: 10 }}>Serviços</h1>
-            <img src="/model_machine.png" alt="modelo de maquina" />
-          </div >
+        <h1>Segmentos</h1>
+        {status.infos.map((info, index) => (
+          <div key={index}>
+            <h2 style={{ marginBottom: 10 }}>{info.subtitle}</h2>
+            <p>{info.description}</p>
+          </div>
+        ))}
+      </div >
     </Container>
   )
 }
