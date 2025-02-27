9726df77ff2d6243142660387c17c1aeafe8e0a3
unknown <marcio.b.a.dias@gmail.com>
2025-02-27 10:11:24 -0300

    💹 Importando tabela de soluções

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import { Container, Section } from './styles'
+import { Container, Section, TableContainer } from './styles'
 import SystemTable from '../TableComponenttest'
 
 const Products = () => {
+  const problems = [
+    'Garçons levando muito tempo para atender',
+    'Pedidos enviados com erros para a cozinha',
+    'Confusão na hora de fechar comanda em grupo',
+    'Clientes que saem sem pagar',
+    'Quebras de caixa no fim do dia',
+  ]
+  
+  const solutions = [
+    'Mais rapidez com PDV móvel e Totens',
+    'Pedidos vinculados à mesa e impressão remota na cozinha',
+    'Comanda individual por cliente',
+    'Possibilidade de cobrança pelo Whatsapp',
+    'Fechamento em 15min com adquirência integrada',
+  ]
+
   return (
     <Container name="products" id="products">
       <Section>
@@ -15,9 +31,28 @@ const Products = () => {
         </div>
       </Section>
       <Section>
-        <div>
-          <SystemTable />
-        </div>
+        <TableContainer>
+      <Table>
+        <Column className="problems">
+          <Title>Problemas na Operação</Title>
+          <List>
+            {problems.map((problem, index) => (
+              <ListItem key={index}>{problem}</ListItem>
+            ))}
+          </List>
+        </Column>
+        <Column className="solutions">
+          <Title>Soluções com o Sistema da Zig</Title>
+          <List>
+            {solutions.map((solution, index) => (
+              <ListItem key={index} className="solution">
+                {solution}
+              </ListItem>
+            ))}
+          </List>
+        </Column>
+      </Table>
+    </TableContainer>
       </Section>
     </Container>
   )
--- src/components/Products/styles.jsx
+++ src/components/Products/styles.jsx
@@ -18,3 +18,57 @@ export const Section = styled.section`
   color: var(--color-primary);
   padding: 50;
 `
+
+export const TableContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  border-radius: 10px;
+`
+
+export const Table = styled.div`
+  display: flex;
+  width: 80%;
+  border-radius: 10px;
+  overflow: hidden;
+`
+
+export const Column = styled.div`
+  flex: 1;
+  padding: 20px;
+  color: white;
+  &.problems {
+    background: #c90000;
+  }
+  &.solutions {
+    background: #009c2a;
+  }
+`
+
+export const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 20px;
+`
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`
+
+export const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  font-size: 18px;
+
+  &::before {
+    content: '\u2716';
+    color: white;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  &.solution::before {
+    content: '\u2714';
+  }
+`
